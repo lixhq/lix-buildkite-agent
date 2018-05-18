459b5249621b5edf15b2f8367391fb8e8aeae1a5
Emil Ingerslev <emil@ingerslev.io>
2018-05-18 13:26:37 +0200

    Remove test code copied from aves

--- .buildkite/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-eval "$($(dirname $0)/env.sh)"
-
-echo "--- Running flow check"
-docker run --rm --entrypoint "" "$DOCKER_IMAGE" yarn run flow check
-echo "--- Running unit tests"
-docker run --rm --entrypoint "" "$DOCKER_IMAGE" yarn run test:unit
